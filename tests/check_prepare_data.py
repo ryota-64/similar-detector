3df0a43fbd21955e70187d712d3afb4d713fb6d3
竹中良太 <canis23t@gmail.com>
2020-12-20 22:47:54 +0900

    アスペクト比を調整

--- tests/check_prepare_data.py
+++ tests/check_prepare_data.py
@@ -18,22 +18,6 @@ if __name__ == '__main__':
     plate_data.set_dynain_data(dynain_data)
     for conter in conter_paths:
         plate_data.set_conter(conter.name, conter)
-    # plate_data.set_dynain_data_old(dynain_data)
-    # for conter in conter_paths:
-    #     print(
-    #         conter.relative_to(opt.raw_data_path).parents[len(conter.relative_to(opt.raw_data_path).parents) - 3].name)
-    #     plate_data.set_conter(
-    #         conter.relative_to(opt.raw_data_path).parents[len(conter.relative_to(opt.raw_data_path).parents) - 3].name,
-    #         conter)
+
     output = plate_data.output(output_size=(256, 256))
     np.save('result/out3.npy', output)
-    # plate_data.plot_normal_vector(save_name='result/vector_sample.png')
-    # plate_label = plate_data.output_labels()
-    print('lo')
-    print(output.shape)
-
-    normal_vec_fig = plate_data.get_normal_vector_fig(figsize=(32, 32))
-
-    from matplotlib import pyplot as plt
-    plt.imshow(normal_vec_fig)
-    plt.show()
